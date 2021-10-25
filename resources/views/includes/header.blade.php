ccc0931972e973f06e610a73b8a7407096512e57
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-10-25 17:54:06 +0700

    header finish

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,18 +2,18 @@
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
-        <span>FlexStart</span>
+        <img width="75px" height="auto" src="assets/img/favicon.png" alt="logo">
       </a>
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a href="blog.html">Blog</a></li>
+          <li><a class="nav-link scrollto {{request()->routeIs('home') ? 'active' : ''}}" href="{{route('home')}}">Home</a></li>
+          <li><a class="nav-link scrollto {{request()->routeIs('service') ? 'active' : ''}}" href="{{route('service')}}">Layanan</a></li>
+          <li><a class="nav-link scrollto {{request()->routeIs('integration') ? 'active' : ''}}" href="{{route('integration')}}">Integrasi</a></li>
+          <li><a class="nav-link scrollto {{request()->routeIs('case_study') ? 'active' : ''}}" href="{{route('case_study')}}">Studi kasus</a></li>
+          <li><a class="nav-link scrollto {{request()->routeIs('corporate') ? 'active' : ''}}" href="{{route('corporate')}}">Korporasi</a></li>
+          <li><a class="nav-link scrollto {{request()->routeIs('about') ? 'active' : ''}}" href="{{route('about')}}">Tentang kami</a></li>
+          {{-- <li><a href="blog.html">Blog</a></li>
           <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
@@ -32,7 +32,7 @@
             </ul>
           </li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+          <li><a class="getstarted scrollto" href="#about">Get Started</a></li> --}}
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->

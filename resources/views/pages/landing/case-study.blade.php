6e1c9b906e5dd5e2e432f20da4483adfe89475c2
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-10-29 00:52:10 +0700

    case study finish

---
+++ resources/views/pages/landing/case-study.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app')
+
+@section('title', 'Case Study')
+
+@push('after-style')
+    
+@endpush
+@section('content')
+<section class="values mt-3">
+
+    <div class="container" data-aos="fade-up">
+
+      <header class="section-header">
+        <p style="text-align: start">Case Study</p>
+      </header>
+
+      <div class="row">
+
+        <p style="margin-top: -1rem" class="col-12 col-xl-8">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+          Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+
+      </div>
+
+    </div>
+
+</section>
+<section class="values">
+
+    <div class="container" data-aos="fade-up">
+        
+      <div class="row">
+        <div class="col-xl-4 text-center" data-aos="fade-right" data-aos-delay="100">
+            <img width="300px" src="assets/img/features-3.png" class="img-fluid p-4" alt="">
+        </div>
+        <div class="col-12 col-xl-8">
+        <header class="section-header">
+            <p style="text-align: start; font-size: 28px">Retail Industry</p>
+        </header>
+        <p style="margin-top: -1rem; font-size: 14px">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+            Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xl-4 text-center d-xl-none" data-aos="fade-right" data-aos-delay="100">
+            <img width="300px" src="assets/img/features-3.png" class="img-fluid p-4" alt="">
+        </div>
+        <div class="col-12 col-xl-8">
+        <header class="section-header">
+            <p style="text-align: start; font-size: 28px">F&B Industry</p>
+        </header>
+        <p style="margin-top: -1rem; font-size: 14px">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+            Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+        </div>
+        <div class="col-xl-4 text-center d-none d-xl-block" data-aos="fade-right" data-aos-delay="100">
+            <img width="300px" src="assets/img/features-3.png" class="img-fluid p-4" alt="">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xl-4 text-center" data-aos="fade-right" data-aos-delay="100">
+            <img width="300px" src="assets/img/features-3.png" class="img-fluid p-4" alt="">
+        </div>
+        <div class="col-12 col-xl-8">
+        <header class="section-header">
+            <p style="text-align: start; font-size: 28px">Management</p>
+        </header>
+        <p style="margin-top: -1rem; font-size: 14px">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+            Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xl-4 text-center d-xl-none" data-aos="fade-right" data-aos-delay="100">
+            <img width="300px" src="assets/img/features-3.png" class="img-fluid p-4" alt="">
+        </div>
+        <div class="col-12 col-xl-8">
+        <header class="section-header">
+            <p style="text-align: start; font-size: 28px">Supply Chain Industry</p>
+        </header>
+        <p style="margin-top: -1rem; font-size: 14px">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+            Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+        </div>
+        <div class="col-xl-4 text-center d-none d-xl-block" data-aos="fade-right" data-aos-delay="100">
+            <img width="300px" src="assets/img/features-3.png" class="img-fluid p-4" alt="">
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-xl-4 text-center" data-aos="fade-right" data-aos-delay="100">
+            <img width="300px" src="assets/img/features-3.png" class="img-fluid p-4" alt="">
+        </div>
+        <div class="col-12 col-xl-8">
+        <header class="section-header">
+            <p style="text-align: start; font-size: 28px">Startup</p>
+        </header>
+        <p style="margin-top: -1rem; font-size: 14px">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+            Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+        </div>
+      </div>
+      
+
+    </div>
+
+</section>
+@endsection

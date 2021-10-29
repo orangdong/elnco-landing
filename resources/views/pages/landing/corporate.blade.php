0ea8949d3deac45f94f62a88fa76941ac006cb3c
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-10-29 18:03:01 +0700

    corporate blade

---
+++ resources/views/pages/landing/corporate.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Corporate GCG')
+@section('content')
+<section class="values mt-5">
+
+    <div class="container" data-aos="fade-up">
+
+      <header class="section-header">
+        <p style="text-align: start">GCG</p>
+      </header>
+
+      <div class="row">
+
+        <p style="margin-top: -1rem" class="col-12 col-xl-8">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+          Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+
+      </div>
+
+    </div>
+
+</section>
+@endsection

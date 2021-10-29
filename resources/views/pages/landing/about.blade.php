d346c6f0e0290999aad76281cf79687749cdb431
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-10-29 18:03:30 +0700

    about & integration blade

--- resources/views/pages/landing/about.blade.php
+++ resources/views/pages/landing/about.blade.php
@@ -2,5 +2,22 @@
 
 @section('title', 'About')
 @section('content')
+<section class="values mt-5">
 
+    <div class="container" data-aos="fade-up">
+
+      <header class="section-header">
+        <p style="text-align: start">Tentang Kami</p>
+      </header>
+
+      <div class="row">
+
+        <p style="margin-top: -1rem" class="col-12 col-xl-8">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+          Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+
+      </div>
+
+    </div>
+
+</section>
 @endsection
--- resources/views/pages/landing/integration.blade.php
+++ resources/views/pages/landing/integration.blade.php
@@ -2,5 +2,93 @@
 
 @section('title', 'Integration')
 @section('content')
+<section class="values mt-5">
 
+    <div class="container" data-aos="fade-up">
+
+      <header class="section-header">
+        <p style="text-align: start">Integrasi</p>
+      </header>
+
+      <div class="row">
+
+        <p style="margin-top: -1rem" class="col-12 col-xl-8">Consequuntur inventore voluptates consequatur aut vel et. Eos doloribus expedita. 
+          Sapiente atque consequatur minima nihil quae aspernatur quo suscipit voluptatem.</p>
+
+      </div>
+
+    </div>
+
+</section>
+
+<section class="services">
+
+    <div class="container" data-aos="fade-up">
+
+      <header class="section-header">
+        <h2>Integrasi</h2>
+        <p>Integrasi dan penggunaan yang mudah </p>
+      </header>
+
+      <div class="row gy-4">
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+          <div class="service-box blue">
+            <i class="ri-whatsapp-line icon"></i>
+            <h3>Whatsapp</h3>
+            <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
+            <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+          <div class="service-box orange">
+            <i class="ri-mail-check-line icon"></i>
+            <h3>Email</h3>
+            <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
+            <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+          <div class="service-box green">
+            <i class="ri-qr-code-line icon"></i>
+            <h3>QR Code</h3>
+            <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
+            <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+          <div class="service-box red">
+            <i class="ri-barcode-line icon"></i>
+            <h3>General barcode</h3>
+            <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
+            <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+          <div class="service-box purple">
+            <i class="ri-fingerprint-line icon"></i>
+            <h3>Fingerprint</h3>
+            <p>Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.</p>
+            <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="700">
+          <div class="service-box pink">
+            <i class="ri-secure-payment-line icon"></i>
+            <h3>Payment Gateway</h3>
+            <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p>
+            <a href="#" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
 @endsection

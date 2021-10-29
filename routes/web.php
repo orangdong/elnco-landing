ab4b8650527be705f1b7c02ebc80ab293e079879
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-10-29 12:41:13 +0700

    route fix

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LandingController::class, 'index'])->name('home');
 
 Route::get('service', [LandingController::class, 'service'])->name('service');
-Route::get('service', [LandingController::class, 'service_addon'])->name('service.add-on');
-Route::get('service', [LandingController::class, 'service_package'])->name('service.package');
+Route::get('service/add-on', [LandingController::class, 'service_addon'])->name('service.add-on');
+Route::get('service/package', [LandingController::class, 'service_package'])->name('service.package');
 
 Route::get('integration', [LandingController::class, 'integration'])->name('integration');
 Route::get('case-study', [LandingController::class, 'case_study'])->name('case_study');
 
 Route::get('corporate', [LandingController::class, 'corporate'])->name('corporate');
-Route::get('corporate', [LandingController::class, 'corporate_csr'])->name('corporate.csr');
+Route::get('corporate/csr', [LandingController::class, 'corporate_csr'])->name('corporate.csr');
 Route::get('about', [LandingController::class, 'about'])->name('about');
